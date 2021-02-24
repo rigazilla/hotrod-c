aa0657ca3172401cbc85755b1cf28e4112887ec7
rigazilla <rigazilla@users.noreply.github.com>
2021-02-24 11:29:15 +0000

    deploy: 1c49bbefdaf4a147599eec4c67489f8085ea4987

--- search/all_7.js
+++ search/all_7.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['readbyte_7',['readByte',['../hotrod-c_8cpp.html#a6bdd5f7bc91f91d8a67bdd6984a01c06',1,'hotrod-c.cpp']]],
   ['readbytes_8',['readBytes',['../hotrod-c_8cpp.html#ae63e5dce10cad2647f1c4e547f3d9f9d',1,'hotrod-c.cpp']]],
-  ['readheader_9',['readHeader',['../hotrod-c_8cpp.html#a1f764dfda352b48e7583a19336edda35',1,'hotrod-c.cpp']]],
+  ['readresponseheader_9',['readResponseHeader',['../hotrod-c_8cpp.html#ae3cb96fadb263e51e027d39b1618b974',1,'hotrod-c.cpp']]],
   ['readvint_10',['readVInt',['../hotrod-c_8cpp.html#ab8ec7d3d664a4afcc35b28dab697a0f1',1,'hotrod-c.cpp']]],
   ['readvlong_11',['readVLong',['../hotrod-c_8cpp.html#a413d0bce7d3c3bb63045faf7487dcf2f',1,'hotrod-c.cpp']]],
   ['request_5fparsing_5ferror_5fstatus_12',['REQUEST_PARSING_ERROR_STATUS',['../group__ErrorResponseCode.html#ga8fb9ea3dcd509c2ea3b3d7cf61fe1019',1,'hotrod-c.cpp']]],
--- search/functions_0.js
+++ search/functions_0.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['readbyte_23',['readByte',['../hotrod-c_8cpp.html#a6bdd5f7bc91f91d8a67bdd6984a01c06',1,'hotrod-c.cpp']]],
   ['readbytes_24',['readBytes',['../hotrod-c_8cpp.html#ae63e5dce10cad2647f1c4e547f3d9f9d',1,'hotrod-c.cpp']]],
-  ['readheader_25',['readHeader',['../hotrod-c_8cpp.html#a1f764dfda352b48e7583a19336edda35',1,'hotrod-c.cpp']]],
+  ['readresponseheader_25',['readResponseHeader',['../hotrod-c_8cpp.html#ae3cb96fadb263e51e027d39b1618b974',1,'hotrod-c.cpp']]],
   ['readvint_26',['readVInt',['../hotrod-c_8cpp.html#ab8ec7d3d664a4afcc35b28dab697a0f1',1,'hotrod-c.cpp']]],
   ['readvlong_27',['readVLong',['../hotrod-c_8cpp.html#a413d0bce7d3c3bb63045faf7487dcf2f',1,'hotrod-c.cpp']]]
 ];
