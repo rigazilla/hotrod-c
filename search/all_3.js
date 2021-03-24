a84e1cb22e977a3c21c67f70fddf4e0f957b9fb6
rigazilla <rigazilla@users.noreply.github.com>
2021-03-24 16:58:08 +0000

    deploy: 661fa03d1689f6b4b750b79d837e6fd36dd4f870

--- search/all_3.js
+++ search/all_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['ok_5fstatus_3',['OK_STATUS',['../group__ErrorResponseCode.html#gaed001361b8d26446658d8cb3288aac11',1,'hotrod-c.cpp']]],
-  ['operation_20code_20for_20response_4',['Operation code for response',['../group__RequestOpCode.html',1,'']]],
-  ['operation_20code_20for_20request_5',['Operation code for request',['../group__ResponseOpCode.html',1,'']]]
+  ['operation_20code_20for_20request_4',['Operation code for request',['../group__RequestOpCode.html',1,'']]],
+  ['operation_20code_20for_20response_5',['Operation code for response',['../group__ResponseOpCode.html',1,'']]]
 ];
